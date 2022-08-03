a0d351ced0e7b816e641f77244566ac643a49418
Nathan Wick <nathanwick2001@gmail.com>
2022-08-02 22:02:05 -0400

    Added Scoreboard, Multiple Rounds Per Game, And Many Minor Improvements

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+/* Configure Game */
 const config = {
     type: Phaser.AUTO,
     parent: 'game',
@@ -19,97 +20,105 @@ const config = {
     }
 };
 
-/*------Values for use------*/
+/* Global Constants */
+const HALF_WORLD_WIDTH = 450;
+const HALF_WORLD_HEIGHT = 370;
+const BALL_START_SPEED_X = 400;
+const BALL_START_SPEED_Y = 300;
+const PADDLE_SPEED = 400;
+const SCORE_TO_WIN = 11;
 
+/* Global Variables */
 let game = new Phaser.Game(config);
-let beginPong = false;
+let gameIsPaused;
 let firstPlayer;
 let secondPlayer;
-let firstPlayerWins;
-let secondPlayerWins;
+let firstPlayerScore;
+let secondPlayerScore;
+let scoreboard;
 let ballBounce;
 
-const PADDLE_SPEED = 300;
-
-/*----Pre Load-----*/
-
+/* Preload Images */
 function preload() {
     this.load.image('theBalls', 'ball.png');
     this.load.image('thePaddles', 'paddle.png');
 }
 
-/*----- Create Ball and Paddles-----*/
-
+/* Create Scene */
 function create() {
+
+    /* Create Ball */
     ballBounce = this.physics.add.sprite(
-        this.physics.world.bounds.width / 2,
-        this.physics.world.bounds.height / 2,
+        HALF_WORLD_WIDTH,
+        HALF_WORLD_HEIGHT,
         'theBalls'
     ); 
-
     ballBounce.setCollideWorldBounds(true);
-    
-
     ballBounce.setBounce(1, 1);
+
+    /* Create First Player */
     firstPlayer = this.physics.add.sprite(
-        this.physics.world.bounds.width - (ballBounce.body.width / 2 + 1),
-        this.physics.world.bounds.height / 2,
+        ballBounce.body.width / 2 + 1,
+        HALF_WORLD_HEIGHT,
         'thePaddles'
     );
     firstPlayer.setImmovable(true);
     firstPlayer.setCollideWorldBounds(true);
     
+    /* Create Second Player */
     secondPlayer = this.physics.add.sprite(
-        ballBounce.body.width / 2 + 1,
-        this.physics.world.bounds.height / 2,
+        this.physics.world.bounds.width - (ballBounce.body.width / 2 + 1),
+        HALF_WORLD_HEIGHT,
         'thePaddles'
     );
     secondPlayer.setImmovable(true);
     secondPlayer.setCollideWorldBounds(true);
+
+    /* Add Colliders */
     this.physics.add.collider(ballBounce, firstPlayer);
     this.physics.add.collider(ballBounce, secondPlayer);
 
+    /* Add Scoreboard */
+    scoreboard = this.add.text(HALF_WORLD_HEIGHT, HALF_WORLD_WIDTH, "Game Starting...");
 
-    firstPlayerWins = this.add.text(this.physics.world.bounds.height / 2, this.physics.world.bounds.width / 2, 'The First Player Has Won The Game!');
-    firstPlayerWins.setVisible(false);
-    firstPlayerWins.setOrigin(.2);
-    secondPlayerWins = this.add.text(this.physics.world.bounds.height / 2, this.physics.world.bounds.width / 2, 'The Second Player Has Won The Game!');
-    secondPlayerWins.setVisible(false);
-    secondPlayerWins.setOrigin(.2);
+    /* LETS GOOOO :) */
+    startNewGame();
 }
 
-/*------Update The ball and paddle for movement-------*/
-
+/* Update Scene */
 function update() {
 
-    if(!beginPong) {
-        let x = 300;
-        let y = 300;
-        ballBounce.setVelocityX(x);
-        ballBounce.setVelocityY(y);
-        beginPong = true;
+    /* Ball Passes Player 1 */
+    if (ballBounce.body.x < firstPlayer.body.x) {
+        if(secondPlayerScore >= (SCORE_TO_WIN - 1)) {
+            /* Player 2 Wins Game */
+            endGame();
+            scoreboard.setText("Player 2 Has Won The Game!");
+        } else {
+            /* Player 2 Wins Round */
+            secondPlayerScore += 1;
+            startNewRound();
+        }
     }
 
-    /*---Ball Touches either side then game over----*/
-
-
-    if (ballBounce.body.x > firstPlayer.body.x) {
-        ballBounce.setVelocityX(0);
-        ballBounce.setVelocityY(0);
-        secondPlayerWins.setVisible(true);
-    }
-
-    if (ballBounce.body.x < secondPlayer.body.x) {
-        ballBounce.setVelocityX(0);
-        ballBounce.setVelocityY(0);
-        firstPlayerWins.setVisible(true);
+    /* Ball Passes Player 2 */
+    if (ballBounce.body.x > secondPlayer.body.x) {
+        if(firstPlayerScore >= (SCORE_TO_WIN - 1)) {
+            /* Player 1 Wins Game */
+            endGame();
+            scoreboard.setText("Player 1 Has Won The Game!");
+        } else {
+            /* Player 1 Wins Round */
+            firstPlayerScore += 1;
+            startNewRound();
+        }
     }
 
     /* Move Player 1 */
-    if(this.input.keyboard.createCursorKeys().up.isDown) {
+    if(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown) {
         /* Move Up */
         firstPlayer.setVelocityY(-1 * PADDLE_SPEED);
-    } else if (this.input.keyboard.createCursorKeys().down.isDown) {
+    } else if(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S).isDown) {
         /* Move Down */
         firstPlayer.setVelocityY(PADDLE_SPEED);
     } else {
@@ -118,24 +127,80 @@ function update() {
     }
 
     /* Move Player 2 */
-    if(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown) {
+    if(this.input.keyboard.createCursorKeys().up.isDown) {
         /* Move Up */
         secondPlayer.setVelocityY(-1 * PADDLE_SPEED);
-    } else if(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S).isDown) {
+    } else if (this.input.keyboard.createCursorKeys().down.isDown) {
         /* Move Down */
         secondPlayer.setVelocityY(PADDLE_SPEED);
     } else {
         /* Stop Moving */
         secondPlayer.setVelocityY(0);
     }
-
-    if(ballBounce.body.velocity.y > PADDLE_SPEED) {
-        ballBounce.body.setVelocityY(PADDLE_SPEED)
-    }
-
-    if(ballBounce.body.velocity.y < -PADDLE_SPEED) {
-        ballBounce.body.setVelocityY(-PADDLE_SPEED);
-    }
-
 }
 
+/* Start A New Game */
+function startNewGame() {
+
+    /* Unpause Game */
+    gameIsPaused = false;
+
+    /* Set Scores */
+    firstPlayerScore = 0;
+    secondPlayerScore = 0;
+    
+    startNewRound();
+}
+
+/* Start A New Round */
+function startNewRound() {
+
+    /* Update The Scoreboard */
+    scoreboard.setText(firstPlayerScore + " | " + secondPlayerScore);
+
+    /* Stop The Ball */
+    ballBounce.setVelocityX(0);
+    ballBounce.setVelocityY(0);
+
+    /* Put The Ball In The Center */
+    ballBounce.setPosition(HALF_WORLD_WIDTH, HALF_WORLD_HEIGHT)
+
+    /* Send The Ball After 2 Seconds (2000 miliseconds) */
+    setTimeout(function() {
+
+        /* Get Random Number To Determine Ball Start Direction */
+        let randomNumber = Math.floor(Math.random() * 4) + 1;
+
+        /* Add Speed To The Ball */
+        switch(randomNumber) {
+            case 1:
+                /* Speed Bottom Right */
+                ballBounce.setVelocityX(BALL_START_SPEED_X);
+                ballBounce.setVelocityY(BALL_START_SPEED_Y);
+                break;
+            case 2:
+                /* Speed Bottom Left */
+                ballBounce.setVelocityX(-BALL_START_SPEED_X);
+                ballBounce.setVelocityY(BALL_START_SPEED_Y);
+                break;
+            case 3:
+                /* Speed Top Left */
+                ballBounce.setVelocityX(-BALL_START_SPEED_X);
+                ballBounce.setVelocityY(-BALL_START_SPEED_Y);
+                break;
+            default:
+                /* Speed Top Right */
+                ballBounce.setVelocityX(BALL_START_SPEED_X);
+                ballBounce.setVelocityY(-BALL_START_SPEED_Y);
+                break;
+        }
+    }, 2000);
+}
+
+/* End The Game */
+function endGame() {
+
+    /* Stop The Ball */
+    ballBounce.setVelocityX(0);
+    ballBounce.setVelocityY(0);
+}
